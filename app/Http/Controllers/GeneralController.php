714d1f753a742c880ee3737d2f9051c2fe24515f
khalcheese <khalidambobot26@gmail.com>
2024-10-24 20:00:18 +0800

    add dashboard analytics📈: tasks status count

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GeneralController extends Controller
 {
     public function dashboard() {
 
-        return inertia('Dashboard');
+        $pendingCount = Auth::user()->tasks()->where('status', 'pending')->count();
+        $in_progressCount = Auth::user()->tasks()->where('status', 'in_progress')->count();
+        $completedCount = Auth::user()->tasks()->where('status', 'completed')->count();
+        $tasksCount = Auth::user()->tasks()->count();
+
+        return inertia('Dashboard', [
+            'pendingCount' => $pendingCount, 
+            'in_progressCount' => $in_progressCount, 
+            'completedCount' => $completedCount, 
+            'tasksCount' => $tasksCount,
+        ]);
     }
 }
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,26 +1,118 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, Link } from "@inertiajs/react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/shadcn/components/ui/card";
+import {
+  ClockIcon,
+  EllipsisHorizontalCircleIcon,
+  CheckCircleIcon,
+  RectangleStackIcon,
+  ArrowLongRightIcon,
+} from "@heroicons/react/16/solid";
 
-export default function Dashboard() {
-    return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard
-                </h2>
-            }
-        >
-            <Head title="Dashboard" />
+export default function Dashboard({
+  pendingCount,
+  in_progressCount,
+  completedCount,
+  tasksCount,
+}) {
+  return (
+    <AuthenticatedLayout
+      header={
+        <h2 className="text-xl font-semibold leading-tight text-gray-800">
+          Dashboard
+        </h2>
+      }
+    >
+      <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
+      <div className="py-5">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+            <div className="p-2 flex gap-4">
+              <Card className="w-[300px]">
+                <CardHeader className="flex flex-row justify-between relative pb-4">
+                  <CardTitle className="text-lg font-medium">
+                    Pending Tasks
+                  </CardTitle>
+                  <ClockIcon className="w-[22px] absolute top-5 right-4 text-amber-500" />
+                </CardHeader>
+                <CardContent>
+                  <span className="text-5xl font-extrabold">
+                    {pendingCount}
+                  </span>
+                </CardContent>
+                <CardFooter className="flex justify-end">
+                  <Link href="#">
+                    <ArrowLongRightIcon className="w-[22px] text-green-700 hover:text-green-400" />
+                  </Link>
+                </CardFooter>
+              </Card>
+
+              <Card className="w-[300px]">
+                <CardHeader className="flex flex-row justify-between relative pb-4">
+                  <CardTitle className="text-lg font-medium">
+                    In Progress Tasks
+                  </CardTitle>
+                  <EllipsisHorizontalCircleIcon className="w-[22px] absolute top-5 right-4 text-blue-500" />
+                </CardHeader>
+                <CardContent>
+                  <span className="text-5xl font-extrabold">
+                    {in_progressCount}
+                  </span>
+                </CardContent>
+                <CardFooter className="flex justify-end">
+                  <Link href="#">
+                    <ArrowLongRightIcon className="w-[22px] text-green-700 hover:text-green-400" />
+                  </Link>
+                </CardFooter>
+              </Card>
+
+              <Card className="w-[300px]">
+                <CardHeader className="flex flex-row justify-between relative pb-4">
+                  <CardTitle className="text-lg font-medium">
+                    Completed Tasks
+                  </CardTitle>
+                  <CheckCircleIcon className="w-[22px] absolute top-5 right-4 text-green-600" />
+                </CardHeader>
+                <CardContent>
+                  <span className="text-5xl font-extrabold">
+                    {completedCount}
+                  </span>
+                </CardContent>
+                <CardFooter className="flex justify-end">
+                  <Link href="#">
+                    <ArrowLongRightIcon className="w-[22px] text-green-700 hover:text-green-400" />
+                  </Link>
+                </CardFooter>
+              </Card>
+
+              <Card className="w-[300px]">
+                <CardHeader className="flex flex-row justify-between relative pb-4">
+                  <CardTitle className="text-lg font-medium">
+                    Total Tasks
+                  </CardTitle>
+                  <RectangleStackIcon className="w-[22px] absolute top-5 right-4 text-green-600" />
+                </CardHeader>
+                <CardContent>
+                  <span className="text-5xl font-extrabold">{tasksCount}</span>
+                </CardContent>
+                <CardFooter className="flex justify-end">
+                  <Link href="#">
+                    <ArrowLongRightIcon className="w-[22px] text-green-700 hover:text-green-400" />
+                  </Link>
+                </CardFooter>
+              </Card>
             </div>
-        </AuthenticatedLayout>
-    );
+          </div>
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  );
 }
