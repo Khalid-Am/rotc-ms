dc7eaac9ee7107f4e3ba364db379845ae6b91c5a
khalcheese <khalidambobot26@gmail.com>
2024-10-26 20:49:57 +0800

    CI build system👷: added dist

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
+  plugins: [
+    laravel({
+      input: "resources/js/app.jsx",
+      refresh: true,
+    }),
+    react(),
+  ],
+
+  build: {
+    outDir: "dist", // Ensure the output directory is set correctly
+    emptyOutDir: true, // Clears the output folder before each build
+  },
 });
