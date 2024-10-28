506f48bdbe1a5766727cdda12e3c4f02dab82de9
khalcheese <khalidambobot26@gmail.com>
2024-10-28 15:32:28 +0800

    (cleaned)🗑️: removed redundant lines of code

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -44,17 +44,6 @@ export default function Register({ queryParams, officers }) {
     setData("officer_id", selectedOfficer.id);
   };
 
-  const onArchive = (officer) => {
-    router.delete(route("officer.destroy", officer.id), {
-      onSuccess: () => {
-        toast({
-          variant: "archived",
-          description: "Officer was archived!",
-        });
-      },
-    });
-  };
-
   return (
     <AuthenticatedLayout
       header={
@@ -160,16 +149,8 @@ export default function Register({ queryParams, officers }) {
                 </div>
 
                 <div className="mt-4 flex items-center justify-end">
-                  {/* <Link
-                    href={route("login")}
-                    className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Already registered?
-                  </Link> */}
-
                   <PrimaryButton className="ms-4" disabled={processing}>
                     Register
-                    {console.log(data)}
                   </PrimaryButton>
                 </div>
               </form>
